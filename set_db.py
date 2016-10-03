7598475dec5b42231ae26aa00f166b28fde6c5c9
mauriciolongato <mauriciolongato@gmail.com>
2016-10-03 09:21:29 -0300

    function to set_db

---
+++ set_db.py
@@ -0,0 +1,14 @@
+import sqlite3 as sql
+
+conn = sql.connect('./twitter_streaming_data.db')
+# Create table that will contain twitter hashtag info
+conn.execute("""CREATE TABLE tweets(
+                id INTEGER PRIMARY KEY
+                , tweet_id INTEGER
+                , insert_date TEXT
+                , created_at TEXT
+                , hashtag TEXT
+                )
+            """)
+
+conn.close()
