c53826bd4b4df0c7c65a2e134bd9c049309e693e
mauriciolongato <mauriciolongato@gmail.com>
2016-11-15 12:31:45 -0200

    function to set_db

--- configkeys.py
+++ configkeys.py
@@ -1,4 +1,12 @@
-keys = {'consumer_key':'',
-        'consumer_secret_key':'',
-        'access_token':'',
-        'access_secret_token':''}
+
+"""
+Insert your twitter app's keys and pass
+Find more information about the keys at:
+    - to get information about: https://dev.twitter.com/streaming/public
+    - in order to get the keys: https://apps.twitter.com/
+"""
+
+keys = {'consumer_key': '',
+        'consumer_secret_key': '',
+        'access_token': '',
+        'access_secret_token': ''}
